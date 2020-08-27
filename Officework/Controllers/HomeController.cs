edaa8dc4db56d5054b8422ed1e922793d84a4f8d
Deepanshu Mehta <deepanshu9811137361@gmail.com>
2020-08-27 21:15:16 +0530

    Added Authentication.cs file

---
+++ Officework/Controllers/HomeController.cs
@@ -0,0 +1,80 @@
+﻿using Officework.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Services.Protocols;
+using System.Web.UI;
+
+namespace Officework.Controllers
+{
+	public class HomeController : Controller
+	{
+		
+		public ActionResult Index()
+		{
+			return View();
+		}
+		[HttpGet]
+		public ActionResult Formdetails()
+		{
+				return View();
+		}
+		[HttpPost]
+		public ActionResult Formdetails(Userdetail1 user)
+		{
+			if (ModelState.IsValid)
+			{
+				FormData1Entities form = new FormData1Entities();
+				form.Userdetail1.Add(user);
+				form.SaveChanges();
+				return RedirectToAction("DisplayUserdetails");
+			}
+			return View();
+		}
+
+		[HttpGet]
+		public ActionResult UpdateDetails(int id)
+		{
+			FormData1Entities form = new FormData1Entities();
+			Userdetail1 user = form.Userdetail1.Find(id);
+			return View(user);
+		}
+
+		[HttpPost]
+
+		public ActionResult UpdateDetails(Userdetail1 user)
+		{
+			if (ModelState.IsValid)
+			{
+				FormData1Entities form = new FormData1Entities();
+				form.Entry(user).State = EntityState.Modified;
+
+				form.SaveChanges();
+				return RedirectToAction("Displayuserdetails");
+			}
+			return View();
+
+		}
+
+		public ActionResult DisplayUserdetails()
+		{
+			FormData1Entities form = new FormData1Entities();
+			return View(form.Userdetail1.ToList());
+		}
+
+		public ActionResult DeleteDetails(int id)
+		{
+			FormData1Entities form = new FormData1Entities();
+			Userdetail1 user = form.Userdetail1.Find(id);
+			form.Userdetail1.Remove(user);
+			form.SaveChanges();
+			return RedirectToAction("Displayuserdetails");
+		}
+
+	}
+}
---
+++ Officework/Controllers/Userdetail1Controller.cs
@@ -0,0 +1,52 @@
+﻿using Officework.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Runtime.Remoting.Messaging;
+using System.Web.Http;
+
+namespace Officework.Controllers
+{
+    public class Userdetail1Controller : ApiController
+    {
+        [AuthenticationFilter]
+        public IEnumerable<Userdetail1> Get()
+        {
+            FormData1Entities form = new FormData1Entities();
+            return form.Userdetail1;
+        }
+        [AuthenticationFilter]
+        public Userdetail1 Get(int id)
+        { 
+            FormData1Entities form = new FormData1Entities();
+            Userdetail1 user = form.Userdetail1.Find(id);
+            return user;
+        }
+        [AuthenticationFilter]
+        public IHttpActionResult Post(Userdetail1 user)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+                FormData1Entities form = new FormData1Entities();
+                form.Userdetail1.Add(user);
+                form.SaveChanges();
+                return CreatedAtRoute("DefaultApi", new { id = user.Id }, user);
+
+
+        }
+
+        public Userdetail1 Delete(int id)
+        {
+            FormData1Entities form = new FormData1Entities();
+            Userdetail1 user= form.Userdetail1.Find(id);
+            form.Userdetail1.Remove(user);
+            form.SaveChanges();
+            return user;
+
+        }
+    }
+}
