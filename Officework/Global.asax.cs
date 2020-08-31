f9dad6993ce0c273745e64bdc6d633b938a677e6
Raghav Khunger <raghav.khunger@gmail.com>
2020-08-31 12:41:02 +0530

    Update Global.asax.cs

--- Officework/Global.asax.cs
+++ Officework/Global.asax.cs
@@ -13,8 +13,8 @@ namespace Officework
     {
         protected void Application_Start()
         {
-            Console.WriteLine("Hello");
-            AreaRegistration.RegisterAllAreas();
+            AreaRegistration.RegisterAllAreas();;//hjkkjgjh
+            //jkhgjhfghjfhgfhgfhgfhgfhg
             GlobalConfiguration.Configure(WebApiConfig.Register);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
